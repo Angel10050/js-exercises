52c7e2bd58c27e1f835a21028443d8a5da29e43b
Velez Angel Mario <50499606+Angel10050@users.noreply.github.com>
2019-08-04 21:30:31 -0500

    revisando W-1
    revisando W-1

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+let porcentageStudiantes = numberOfStudents * 100 / 23
+let roundi = Math.round(porcentageStudiantes)
+
+let porcentageMentores = numberOfMentors * 100 / 23
+let round = Math.round(porcentageMentores)
+console.log('Number of student ' + roundi + '%')
+console.log('Number of mentors ' + round + '%')
+
